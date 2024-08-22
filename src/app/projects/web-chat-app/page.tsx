d515ce7045262ff1d982dc71ff77b1c659c2adb8
Jason Louie <jason.louie29@myhunter.cuny.edu>
2024-08-22 18:59:02 -0400

    Fix footer

---
+++ src/app/projects/web-chat-app/page.tsx
@@ -0,0 +1,7 @@
+export default function WebChatApp() {
+    return (
+        <div className="container">
+            <p>Test</p>
+        </div>
+    );
+}
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
     return (
         <footer className="bg-n-7 text-white p-4 text-center text-sm md:text-md lg:text-lg">
-            <p>&copy; 2024 Jason Louie's Website. All rights reserved.</p>
+            <p>&copy; 2024 Jason Louie&rsquo;s Website. All rights reserved.</p>
         </footer>
     );
 }
